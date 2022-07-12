1ff5ab7d9da687cb886f14def1d5a906e1050624
Satya <satyap257@gmail.com>
2022-07-12 16:14:12 +0530

    "New changes to index.js"

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect, useState } from "react";
 function App() {
+
+  const [latLong, setLatLong] = useState({});
+  const [weatherData, setWeatherData] = useState({});
+  const [foreCast, setForeCast] = useState([]);
+
+  function getWeatherData(latLon){
+    fetch(process.env.REACT_APP_API_URL +`&lat=${latLon.lat}&lon=${latLon.long}`)
+    .then(res=>res.json())
+    .then(data=>setWeatherData(data.data[0]));
+    getForeCast(latLon);
+  }
+  
+  function getForeCast(latLon){
+    const URL = process.env.REACT_APP_FORECAST_URL + `&lat=${latLon.lat}&lon=${latLon.long}`;
+    fetch(URL)
+    .then(res=>res.json())
+    .then(data=>setForeCast(data.data));
+  }
+
+  function getLatLong(){
+    if(localStorage.latLong){ 
+      setLatLong(JSON.parse(localStorage.latLong));
+      const latLon = JSON.parse(localStorage.latLong);
+      getWeatherData(latLon);
+    }else{
+      navigator.geolocation.getCurrentPosition(position => {
+      setLatLong({lat: position.coords.latitude, long: position.coords.longitude});
+      localStorage.setItem('latLong', JSON.stringify(latLong));
+    });
+   }
+  }
+  useEffect(()=>{
+    getLatLong();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-full h-full p-10 text-white">
+      <div className="flex flex-col justify-between">
+        <div className="text-2xl">{weatherData?.city_name}</div>
+        <div className="flex flex-row align-center w-full">
+          <div className="text-4xl">{weatherData?.app_temp}<span><sup>o</sup>C</span></div>
+          <div className="text-xl">{weatherData?.weather?.description}</div>
+          <div><img className="md:w-[60px] md:h-[60px] w-full h-full" src={`https://www.weatherbit.io/static/img/icons/${weatherData?.weather?.icon}.png`}></img></div>
+        </div>
+      </div>
+      <div className="w-full">
+        <table className="table-auto">
+          <thead>
+            <tr>
+              <td className="m-2 p-1 text-center">Date</td>
+              <td className="m-2 p-1 text-center">Temperature</td>
+              <td className="m-2 p-1 text-center">Weather</td>
+            </tr>
+          </thead>
+          <tbody>
+            {foreCast?.slice(0,6).map((item, index)=>{
+              return <tr key={index}>
+                <td className="m-2 text-center">{item.datetime}</td>
+                <td className="m-2 text-center">{item.temp}</td>
+                <td className="flex flex-row justify-around m-2 md:text-center"><img alt={item?.weather.description} width="60" height="60" src={`https://www.weatherbit.io/static/img/icons/${item?.weather?.icon}.png`} /><div className="text-center">{item.weather.description}</div></td>
+              </tr>
+            })}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
